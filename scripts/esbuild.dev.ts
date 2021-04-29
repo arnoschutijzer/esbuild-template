885866012a7881e857126ba6fdd0ce569d1aa9da
Arno Schutijzer <arno.schutijzer@skryv.com>
2021-04-29 22:24:18 +0200

    add proxy

--- scripts/esbuild.dev.ts
+++ scripts/esbuild.dev.ts
@@ -1,23 +1,30 @@
-import sharedConfig from './esbuild.shared';
-import { build } from 'esbuild';
-import { start } from 'live-server';
+import sharedConfig from "./esbuild.shared";
+import { build } from "esbuild";
+import { start } from "live-server";
+
+const {
+  FRONTEND_PORT = 9000,
+  PROXY_PORT = 8080,
+  ENABLE_PROXY = true,
+} = process.env;
 
 build({
   ...sharedConfig,
   define: {
-    'process.env.NODE_ENV': '"development"'
+    "process.env.NODE_ENV": '"development"',
   },
   watch: {
     onRebuild(error, result) {
-      if (error) console.error('watch build failed:', error)
-      else console.error('watch build succeeded. warnings:', result.warnings)
-    }
-  }
+      if (error) console.error("watch build failed:", error);
+      else console.error("watch build succeeded. warnings:", result.warnings);
+    },
+  },
 });
 
 start({
-  port: 8080,
+  port: FRONTEND_PORT,
   open: true,
-  root: 'public',
-  file: 'index.html'
+  root: "public",
+  file: "index.html",
+  proxy: ENABLE_PROXY ? [["/api", `http://127.0.0.1:${PROXY_PORT}/api`]] : [],
 });
